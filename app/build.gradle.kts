1b14c5070434020185141e2d84dc432073dde43e
AndreeMeilio <meilioandree27@gmail.com>
2025-05-12 02:13:09 +0700

    changing minimal sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.andreemeilio.suaraku"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.andreemeilio.suaraku"
